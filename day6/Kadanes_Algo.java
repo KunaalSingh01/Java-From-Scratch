09f9b3b2480aeacced6703d6b2f5db96e5d6122c
Kunaal Singh <kunaalkaushal004@gmail.com>
2026-01-18 20:13:56 +0530

    Kadane's algorithm for printing max subarray sum

---
+++ day6/Kadanes_Algo.java
@@ -0,0 +1,31 @@
+// Using Kandane's Algorithm to check the maximum sum of the sub array
+//Time Complexity=O(n)
+//It states that if sum of two numbers = -ve number ; then sum = 0; 
+// does not work for arrays with all -ve values
+
+public class Kadanes_Algo {
+    public static void kadaneAlgo(int array[]){
+        
+        int maxSum=Integer.MIN_VALUE;
+        int currentSum=0;
+
+        for(int i=0; i<array.length;i++){
+            //currentSum=array[i]+currentSum;
+            // if(currentSum<0){
+            //     currentSum=0;
+            // }
+            //for handling arrays with all -ve values
+            currentSum=Math.max(array[i], array[i]+currentSum);
+            
+            maxSum=Math.max(currentSum, maxSum);
+        }
+        
+        System.out.println("Max sum of sub arrays = "+ maxSum);
+    }
+
+    public static void main(String[] args) {
+        int numbers[]={-2,-3,-7,-34,-8};
+        kadaneAlgo(numbers);
+    }
+}
+
