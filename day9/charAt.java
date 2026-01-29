f65655b76d59b158b8fc359c0a005bb2d943236a
Kunaal Singh <kunaalkaushal004@gmail.com>
2026-01-29 12:29:46 +0530

    learning to get the characters from a string in java

---
+++ day9/charAt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class charAt {
+    public static void getCharacter(String str){
+        for (int i=0; i<str.length();i++){
+            System.out.print(str.charAt(i)+" ");
+        }
+    }
+    public static void main(String[] args) {
+        String fullName="kunaal kaushal Singh";
+        getCharacter(fullName);
+    }
+}
