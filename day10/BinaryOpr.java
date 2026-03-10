60f9fcc1dc04ed1f9d1949b479016955ce868f8c
Kunaal Singh <kunaalkaushal004@gmail.com>
2026-03-10 09:01:59 +0530

    Starting bit manupilation in java

---
+++ day10/BinaryOpr.java
@@ -0,0 +1,11 @@
+public class BinaryOpr{
+    public static void main(String[] args) {
+        System.out.println(5&4); // Binary AND 
+        System.out.println(5|4); // Binary OR
+        System.out.println(5^4); // Binary XOR
+        System.out.println(~7);  // Binary One's Compliment (NOT).
+        System.out.println(8<<3); // Binary LeftShift (a<<b = a*2^b) 
+        System.out.println(7>>2); // Binary RightShift (a>>b = b/2^b)
+
+    }
+}
