3986825bf9ecfde31d191d5eabd1be804a119206
KunaalSingh01 <167495447+KunaalSingh01@users.noreply.github.com>
2026-08-05 17:39:38 +0530

    Divide and conquer algos

---
+++ DivideConquer/mergeSort.java
@@ -0,0 +1,22 @@
+
+public class mergeSort {
+    
+    public static void printArr(int arr[]){
+        for (int i=0; i<=arr.length-1;i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static void mergeSorting(int arr[], int si, int ei){
+        
+
+    }
+
+    public static void main(String arg[]){
+        int arr[]={6,3,9,5,2,8};
+        mergeSorting(arr,0, arr.length-1);
+        
+
+    }
+}
